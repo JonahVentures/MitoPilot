d46812bed5e4a3294b3ca0116a67abc2ded59610
Daniel.MacGuigan <Daniel.MacGuigan@nmfs.local>
2025-01-02 12:11:21 -0500

    width adjustment

--- R/app_annotate.R
+++ R/app_annotate.R
@@ -65,14 +65,13 @@ annotate_server <- function(id) {
           rowStyle = rt_highlight_row(),
           defaultColDef = colDef(align = "left", show = FALSE),
           columns = list(
-            `.selection` = colDef(show = T, sticky = "left", width = 28),
+            `.selection` = colDef(show = T, sticky = "left", minWidth = 28),
             annotate_lock = colDef(
               show = TRUE,
               sticky = "left",
               name = "",
               html = TRUE,
-              filterable = FALSE,
-              width = 32,
+              minWidth = 32,
               align = "center",
               cell = rt_dynamicIcon(
                 c(
@@ -86,8 +85,7 @@ annotate_server <- function(id) {
               sticky = "left",
               name = "",
               html = TRUE,
-              filterable = FALSE,
-              width = 30,
+              minWidth = 30,
               align = "center",
               cell = rt_dynamicIcon(
                 c(
@@ -116,14 +114,14 @@ annotate_server <- function(id) {
               show = TRUE,
               name = "Annotate Opts.",
               html = TRUE,
-              width = 130,
+              minWidth = 130,
               cell = rt_link(ns("set_annotate_opts"))
             ),
             curate_opts = colDef(
               show = TRUE,
               name = "Curate Opts.",
               html = TRUE,
-              width = 110,
+              minWidth = 110,
               cell = rt_link(ns("set_curate_opts"))
             ),
             length = colDef(
@@ -146,7 +144,7 @@ annotate_server <- function(id) {
               name = "Last Updated",
               filterable = FALSE,
               html = T,
-              width = 150,
+              minWidth = 150,
               cell = rt_ts_date()
             ),
             annotate_notes = colDef(
@@ -164,7 +162,7 @@ annotate_server <- function(id) {
               filterable = FALSE,
               name = "",
               html = TRUE,
-              width = 80,
+              minWidth = 80,
               align = "center",
               cell = rt_icon_bttn_text(ns("details"), "fas fa-square-arrow-up-right fa-xs")
             ),
@@ -174,7 +172,7 @@ annotate_server <- function(id) {
               filterable = FALSE,
               name = "",
               html = TRUE,
-              width = 80,
+              minWidth = 80,
               align = "center",
               cell = rt_icon_bttn_text(ns("output"), "fas fa-folder-open fa-xs")
             )
